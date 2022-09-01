835f446b5ea50e0a60b81c23a96db410992be5e5
Wan2302 <erwan.cherel35@hotmail.com>
2022-09-01 11:19:02 +0200

    read README for details

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
+let input = prompt('Grid size?');
+let numberDivs = parseInt(input) * parseInt(input);
+
+
+function gridSize(){
+    document.querySelector('.container').style.gridTemplateColumns = `repeat(${input}, 1fr)`;
+    document.querySelector('.container').style.gridTemplateRows = `repeat(${input}, 1fr)`;
+}
+gridSize();
+
 function grid(){
-    for(let i=0; i<256; i++){
+    // let gridSize = 
+    for(let i=0; i<numberDivs; i++){
         const div = document.createElement('div');
-        div.setAttribute('class', 'divs');
-        const container = document.querySelector('#container');
+        div.className = 'divs';
+        const container = document.querySelector('.container');
         container.appendChild(div);
     }
 }
 grid();
 
-const container = document.getElementById('container');
-const divBlack = document.querySelector('.divs');
+const container = document.querySelector('.container');
+const divHover = document.querySelector('.divs');
 container.addEventListener('mouseover', (divs) => {
-    divs.target.style.backgroundColor = 'black';
-});
+    divs.target.style.backgroundColor = "black";
+})
+
