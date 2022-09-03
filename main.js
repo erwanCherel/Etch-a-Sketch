51ed4282807cf765dd4fb80c8575c534d553d1c5
Wan2302 <erwan.cherel35@hotmail.com>
2022-09-03 18:04:06 +0200

    modified

---
+++ main.js
@@ -0,0 +1,58 @@
+// random color = `hsl(${Math.random() * 360}, 100%, 50%)`
+
+
+function populateGrid(size){
+    let container = document.querySelector("#container");
+    container.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
+    container.style.gridTemplateRows = `repeat(${size} , 1fr)`;
+
+    let amount = size * size;
+    for(i=0; i<amount;i++){
+        let div = document.createElement("div");
+        container.insertAdjacentElement("beforeend", div);
+    }
+
+    container.addEventListener('mouseover', (div) => {
+        div.target.style.backgroundColor = "black";
+    })
+}
+populateGrid(16);
+
+function changeSize(input) {
+    if (input >= 2 && input <= 100) {
+      document.querySelector(".error").style.display = "none";
+      populateGrid(input);
+      resetGrid()
+    } else {
+      document.querySelector(".error").style.display = "grid";
+    }
+  }
+
+
+function resetGrid(){
+  let container = document.querySelector("#container");
+  let divs = container.querySelectorAll("div");
+  divs.forEach((div) => (div.style.backgroundColor = "orange"));
+}
+
+function black(){
+  container.addEventListener('mouseover', (div) => {
+    div.target.style.backgroundColor = "black";
+})}
+
+function grey(){
+  container.addEventListener('mouseover', (div) => {
+    div.target.style.backgroundColor = "grey";
+})}
+
+function random(){
+  container.addEventListener('mouseover', (div) => {
+    div.target.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+})
+}
+
+function eraser(){
+  container.addEventListener('mouseover', (div) => {
+    div.target.style.backgroundColor = "orange";
+})
+}
